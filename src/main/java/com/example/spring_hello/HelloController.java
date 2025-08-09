06ab7262c732b62dbec4abda32a3b79dc1ebc074
Suhana786-s <suhanaziya786@gmail.com>
2025-08-09 14:29:06 +0530

    modification 1

--- src/main/java/com/example/spring_hello/HelloController.java
+++ src/main/java/com/example/spring_hello/HelloController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/")
     public String hello(){
-        return "Hello World from Spring Boot";
+        return "Suhana is my name";
     }
     }
     
